a38454275ae3e7a73b9bfecfaef6a7a30772584f
ValeriaGuty <a2020112787@estudiantes.upsa.edu.bo>
2021-10-13 18:17:37 -0400

    Baraja creada

---
+++ ValeriaGutierrez/Controllers/BarajaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValeriaGutierrez.Data;
+using ValeriaGutierrez.Models;
+
+namespace ValeriaGutierrez.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarajaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BarajaController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Naipe>> GetCancion()
+        {
+            var list = await _context.Naipe.ToListAsync();
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+            var cancion = list[index];
+
+            if (cancion == null)
+            {
+                return NoContent();
+            }
+            return cancion;
+        }
+    }
+}
