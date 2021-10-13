75ab3286467d0d2b02abb37ebbc6bfde278b7130
ValeriaGuty <a2020112787@estudiantes.upsa.edu.bo>
2021-10-13 17:52:31 -0400

    Modelo creado

---
+++ ValeriaGutierrez/Models/Naipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValeriaGutierrez.Models
+{
+    public class Naipe
+    {
+        [Key]
+        public String NaipeId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 4, ErrorMessage = "La longitud debe estar entre {4} y {200}")]
+        public String Nombre { get; set; }
+        [Url]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public String Imagen { get; set; }
+
+    }
+}
